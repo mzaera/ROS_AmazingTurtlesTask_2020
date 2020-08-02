2cd25c131a9cc026083c1f83970c81d3ade138a8
mzaera <martizaera99@gmail.com>
2020-08-02 17:06:08 +0100

    v2_normal_Part1 DONE with classes

---
+++ src/v2_normal_Part1.cpp
@@ -0,0 +1,52 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+
+
+
+class Listener
+{
+private:
+
+    ros::NodeHandle n;
+	ros::Subscriber sub;
+
+public:
+    Listener(){
+
+        this->n = ros::NodeHandle();
+        this->sub = n.subscribe("name_topic", 1000, &Listener::printing_Callback, this );
+
+    }
+
+    void run(){
+
+        ros::Rate loop_rate(10);
+
+        while (ros::ok())
+        {
+
+            ros::spinOnce();
+            loop_rate.sleep();
+        }
+    }
+
+    void printing_Callback(const std_msgs::String::ConstPtr& msg)
+	{
+		ROS_INFO("I heard: [%s]", msg->data.c_str());
+
+	}
+};
+
+
+int main(int argc, char **argv){
+
+    ros::init(argc, argv, "v2_normal_Part1");
+
+    auto controller = Listener();
+    controller.run();
+
+
+    return 0;
+}
+
