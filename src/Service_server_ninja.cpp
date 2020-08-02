0078308a1f13622b9e65564cb56067d334cf6fe9
mzaera <martizaera99@gmail.com>
2020-08-02 10:54:10 +0100

    ninja done (no classes)

--- src/Service_server_ninja.cpp
+++ src/Service_server_ninja.cpp
@@ -5,41 +5,24 @@
 
 #include <std_srvs/SetBool.h>
 
+bool service_bool;
+geometry_msgs::Twist guardat;
 
-bool callback(std_srvs::SetBool::Request& request, std_srvs::SetBool::Response& response)
+bool Service_callback(std_srvs::SetBool::Request& request, std_srvs::SetBool::Response& response)
 {
-
-    ros::NodeHandle nh;
-    ros::Publisher bool_pub = nh.advertise<std_msgs::String>("bool_topic", 1000);
-    
-    ros::Rate loop_rate(1);
-    ros::Time start_time = ros::Time::now();
-    ros::Duration transcorregut = ros::Time::now() - start_time;
-    ros::Duration periode(1.0); 
-
-    std_msgs::String test_num522;
-
   if(request.data){
-
-    test_num522.data= "TRUE";
-
+   service_bool = true;
   }else{
-
-    test_num522.data= "FALSE";
-
+    service_bool = false ;
   }
-
-  while(transcorregut < periode){
-
-    transcorregut = ros::Time::now() - start_time;
-
-    bool_pub.publish(test_num522);
-
-    loop_rate.sleep();
-  }
-
 
   return true;
+}
+
+
+void Move_callback(const geometry_msgs::Twist rebut)
+{
+  guardat=rebut;
 }
 
 
@@ -48,9 +31,25 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "Service_server_ninja");
   ros::NodeHandle n;
 
-  ros::ServiceServer service = n.advertiseService("Service_server_ninja", callback);
+  ros::ServiceServer service = n.advertiseService("Service_server_ninja", Service_callback);
 
-  ros::spin();
+  ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1000);
+  auto msg = geometry_msgs::Twist();
+
+  ros::Subscriber sub = n.subscribe("keyboard/cmd_vel", 1000, Move_callback);
+
+  ros::Rate loop_rate(10);
+
+  while (ros::ok())
+  {
+
+    if(service_bool){
+      cmd_vel_pub.publish(guardat);
+    }
+
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
 
   return 0;
 }
