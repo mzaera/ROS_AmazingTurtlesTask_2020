19040351fb2883a8c0a1870b9fd1728030db5026
mzaera <martizaera99@gmail.com>
2020-08-03 15:05:55 +0100

    correct srv turtle generation

--- src/v2_race_service.cpp
+++ src/v2_race_service.cpp
@@ -19,18 +19,29 @@ private:
 
     ros::NodeHandle nh;
     ros::Publisher cmd_vel;
-    
-    geometry_msgs::Twist msg;
+    ros::Subscriber sub;
 
-      
+    turtlesim::Pose actual_pose;
 
 public:
-    Turtle()
+    Turtle(std::string sub_topic, std::string pub_topic)
     {
         this->nh = ros::NodeHandle();
-        this->cmd_vel =  nh.advertise<geometry_msgs::Twist>("player1/cmd_vel", 1000);
-        
+
+        this->sub = nh.subscribe(sub_topic, 1000, &Turtle::pose_Callback, this );
+        this->cmd_vel =  nh.advertise<geometry_msgs::Twist>(pub_topic, 1000);
+
         ros::Duration(0.1).sleep();
+    }
+
+    void pose_Callback(const turtlesim::PoseConstPtr& pose){
+        this->actual_pose.x = pose->x;
+    }
+
+    void run(){
+
+
+
     }
 
 };  
@@ -40,6 +51,8 @@ public:
 /*------------------------------------------Not more in object part--------------------------------------*/
 
 bool service_bool = false;
+bool start = true;
+bool need_to_kill;
 
 bool Service_callback(std_srvs::SetBool::Request& request, std_srvs::SetBool::Response& response)
 {
@@ -56,6 +69,26 @@ bool Service_callback(std_srvs::SetBool::Request& request, std_srvs::SetBool::Re
     return true;
 }
 
+void kill(std::string turtle_name){
+    ros::NodeHandle n_tokill;
+    ros::ServiceClient client = n_tokill.serviceClient<turtlesim::Kill>("kill");
+    turtlesim::Kill srv;
+    srv.request.name = turtle_name ;
+    client.call(srv);
+}
+
+void spawn(float x, float y, std::string turtle_name){
+    ros::NodeHandle n_tospawn;
+    ros::ServiceClient client = n_tospawn.serviceClient<turtlesim::Spawn>("spawn");
+    turtlesim::Spawn srv;
+    srv.request.x = x;
+    srv.request.y = y;
+    srv.request.theta = 0;
+    srv.request.name = turtle_name;
+
+    client.call(srv);
+    }
+
 
 int main(int argc, char **argv){
 
@@ -64,16 +97,32 @@ int main(int argc, char **argv){
 
     ros::ServiceServer service= n.advertiseService("v2_race_service", Service_callback);
 
-    auto player1 = Turtle();
-    auto player2 = Turtle();
+    auto player1 = Turtle("player1/pose", "player1/cmd_vel");
+    auto player2 = Turtle("player2/pose", "player2/cmd_vel");
+
 
     ros::Rate loop_rate(10);
     while (ros::ok())
     {
         if(service_bool){
+            if (start){
+                kill("turtle1");
+                spawn(1.0,7.5,"player1");
+                spawn(1.0,5.0,"player2");
+                start = false;
+                need_to_kill = true;
+            }
+
+            player1.run();
+            player2.run();
 
 
-
+        }else if (need_to_kill){
+            kill("player1");
+            kill("player2");
+            spawn(5.544445,5.544445,"turtle1");
+            start = true;
+            need_to_kill = false;
 
         }
 
