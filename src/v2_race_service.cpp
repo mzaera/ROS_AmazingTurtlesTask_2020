df0a689eb3ca55f1a26237ac1aa74a755cfb7580
mzaera <martizaera99@gmail.com>
2020-08-03 16:43:38 +0100

    i'll start using delete :)

--- src/v2_race_service.cpp
+++ src/v2_race_service.cpp
@@ -12,6 +12,7 @@
 
 #include <std_srvs/SetBool.h>
 
+
 /*--------------------------------------------------Object part-------------------------------------------*/
 
 class Turtle{
@@ -21,16 +22,21 @@ private:
     ros::Publisher cmd_vel;
     ros::Subscriber sub;
 
+    std::string name;
     turtlesim::Pose actual_pose;
+    geometry_msgs::Twist msg;
+
+    bool print;
 
 public:
-    Turtle(std::string sub_topic, std::string pub_topic)
+    Turtle(std::string turtle_name, std::string sub_topic, std::string pub_topic)
     {
         this->nh = ros::NodeHandle();
-
+        this->name = turtle_name;
         this->sub = nh.subscribe(sub_topic, 1000, &Turtle::pose_Callback, this );
         this->cmd_vel =  nh.advertise<geometry_msgs::Twist>(pub_topic, 1000);
 
+        this->print =false;
         ros::Duration(0.1).sleep();
     }
 
@@ -39,12 +45,39 @@ public:
     }
 
     void run(){
-
-
+        if(this->actual_pose.x < 7.0 ){
+            this->msg.linear.x = this->RandomFloat( 0.5, 2.5 );
+            this->cmd_vel.publish(this->msg);
+        }else if (!this->print){
+            this->msg.linear.x = 0.0;
+            this->cmd_vel.publish(this->msg);
+            ROS_INFO("hola mundo");
+            this->print=true;
+        }else{
+            this->msg.linear.x = 0.0;
+            this->cmd_vel.publish(this->msg);    
+        }
 
     }
 
-};  
+    void empty(){
+
+    }
+
+    void restart(){
+        this->print=false;
+        this->msg.linear.x=0.0;
+        this->actual_pose.x=0.0;
+    }
+
+    float RandomFloat(float a, float b) {
+        float random = ((float) rand()) / (float) RAND_MAX;
+        float diff = b - a;
+        float r = random * diff;
+        return a + r;
+    }
+
+};      
 
 
 
@@ -87,6 +120,7 @@ void spawn(float x, float y, std::string turtle_name){
     srv.request.name = turtle_name;
 
     client.call(srv);
+
     }
 
 
@@ -97,31 +131,43 @@ int main(int argc, char **argv){
 
     ros::ServiceServer service= n.advertiseService("v2_race_service", Service_callback);
 
-    auto player1 = Turtle("player1/pose", "player1/cmd_vel");
-    auto player2 = Turtle("player2/pose", "player2/cmd_vel");
+    auto player1 = Turtle("player1","player1/pose", "player1/cmd_vel");
+    auto player2 = Turtle("player2","player2/pose", "player2/cmd_vel");
+
+    bool turt1=false;
+    bool turt2=false;
 
 
-    ros::Rate loop_rate(10);
+    ros::Rate loop_rate(1000);
     while (ros::ok())
     {
         if(service_bool){
             if (start){
                 kill("turtle1");
-                spawn(1.0,7.5,"player1");
-                spawn(1.0,5.0,"player2");
+                spawn(1.0,6.54,"player1");
+                spawn(1.0,4.54,"player2");
+
+                ros::Duration(0.25).sleep();
+                player1.empty();
+                player2.empty();
+                ros::Duration(0.25).sleep();
+
                 start = false;
                 need_to_kill = true;
             }
 
-            player1.run();
-            player2.run();
-
+        player1.run();
+        player2.run();
+       
 
         }else if (need_to_kill){
+            player1.restart();
+            player2.restart();
             kill("player1");
             kill("player2");
             spawn(5.544445,5.544445,"turtle1");
             start = true;
+
             need_to_kill = false;
 
         }
